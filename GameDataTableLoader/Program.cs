78b3118b5ee4b61ab6d4fb4694368966dec0eae7
kimseonhong <sudal693@naver.com>
2024-01-10 19:31:57 +0900

    [김선홍] Program.cs 제거 예정

--- GameDataTableLoader/Program.cs
+++ GameDataTableLoader/Program.cs
@@ -1,26 +1,26 @@
-﻿using GameDataTableLoader.Exporter;
-using GameDataTableLoader.Loader;
-using GameDataTableLoader.Serializer;
-using OfficeOpenXml;
+﻿//using GameDataTableLoader.Exporter;
+//using GameDataTableLoader.Loader;
+//using GameDataTableLoader.Serializer;
+//using OfficeOpenXml;
 
-namespace GameDataTableLoader
-{
-	public class Program
-	{
-		//public const string ProjectPath = @$"..\..\..\..\..\GameTable";
+//namespace GameDataTableLoader
+//{
+//	public class Program
+//	{
+//		//public const string ProjectPath = @$"..\..\..\..\..\GameTable";
 
-		static void Main(string[] args)
-		{
-			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+//		static void Main(string[] args)
+//		{
+//			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-			//var binary = File.ReadAllBytes(@$"{Program.ProjectPath}\GameTable.pack");
-			//var data = DataTableSerializer.Deserialize<Table.GameData>(binary);
+//			//var binary = File.ReadAllBytes(@$"{Program.ProjectPath}\GameTable.pack");
+//			//var data = DataTableSerializer.Deserialize<Table.GameData>(binary);
 
-			//var tableLoader = new TableLoader<Table.GameData>(DataTableSerializer.Serialize<Table.GameData>, DataTableSerializer.Deserialize<Table.GameData>);
-			//tableLoader.Run();
+//			//var tableLoader = new TableLoader<Table.GameData>(DataTableSerializer.Serialize<Table.GameData>, DataTableSerializer.Deserialize<Table.GameData>);
+//			//tableLoader.Run();
 
-			//var tableExcporter = new TableExporter<Table.GameData>();
-			//tableExcporter.Run();
-		}
-	}
-}
+//			//var tableExcporter = new TableExporter<Table.GameData>();
+//			//tableExcporter.Run();
+//		}
+//	}
+//}
